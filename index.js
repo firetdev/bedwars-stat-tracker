f461f9e26a270ba9e2a9b7bf8a9799e68d3f9a4f
flesheatindragon <futuregamesstore@gmail.com>
2024-10-23 17:34:32 -0500

    Reformat code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//Setup for new users
-if (parseInt(localStorage.getItem("games")) == null || parseInt(localStorage.getItem("games")) == "null" || isNaN(parseInt(localStorage.getItem("games")))) {
-	localStorage.setItem("games", 0);
+
+// Setup for new users
+if (parseInt(localStorage.getItem('games')) == null || parseInt(localStorage.getItem('games')) == 'null' || isNaN(parseInt(localStorage.getItem('games')))) {
+	localStorage.setItem('games', 0);
 	var days = {
 		a: 0,
 		b: 0,
@@ -10,35 +11,38 @@ if (parseInt(localStorage.getItem("games")) == null || parseInt(localStorage.get
 		f: 0,
 		g: 0
 	};
-	localStorage.setItem("days", JSON.stringify(days));
+	localStorage.setItem('days', JSON.stringify(days));
 	var date = new Date();
 	var lastUse = {
 		d: date.getDate(),
 		m: date.getMonth(),
 		y: date.getYear()
 	};
-	localStorage.setItem("lastUse", JSON.stringify(lastUse));
+	localStorage.setItem('lastUse', JSON.stringify(lastUse));
 }
-//Create array of games
+
+// Create array of games
 var games = [];
-for (var i = 0; i < parseInt(localStorage.getItem("games")); i++) {
+for (var i = 0; i < parseInt(localStorage.getItem('games')); i++) {
 	games.push(JSON.parse(localStorage.getItem(i + 1)));
 }
-//Setup data
+
+// Setup data
 var totalkills = 0;
 var totalfinalkills = 0;
 var totalwins = 0;
 var totalbeds = 0;
 var winrate;
-var days = JSON.parse(localStorage.getItem("days"));
+var days = JSON.parse(localStorage.getItem('days'));
 var date = new Date();
-//Checks if it's a new day
+
+// Checks if it's a new day
 var newUse = {
 	d: date.getDate(),
 	m: date.getMonth(),
 	y: date.getYear()
 };
-if (newUse.y > JSON.parse(localStorage.getItem("lastUse")).y || newUse.m > JSON.parse(localStorage.getItem("lastUse")).m) {
+if (newUse.y > JSON.parse(localStorage.getItem('lastUse')).y || newUse.m > JSON.parse(localStorage.getItem('lastUse')).m) {
 	days.a = days.b;
 	days.b = days.c;
 	days.c = days.d;
@@ -46,9 +50,9 @@ if (newUse.y > JSON.parse(localStorage.getItem("lastUse")).y || newUse.m > JSON.
 	days.e = days.f;
 	days.f = days.g;
 	days.g = 0;
-	console.log("NEWDAY");
+	console.log('NEWDAY');
 }
-if (newUse.d > JSON.parse(localStorage.getItem("lastUse")).d && newUse.m == JSON.parse(localStorage.getItem("lastUse")).m) {
+if (newUse.d > JSON.parse(localStorage.getItem('lastUse')).d && newUse.m == JSON.parse(localStorage.getItem('lastUse')).m) {
 	days.a = days.b;
 	days.b = days.c;
 	days.c = days.d;
@@ -56,7 +60,7 @@ if (newUse.d > JSON.parse(localStorage.getItem("lastUse")).d && newUse.m == JSON
 	days.e = days.f;
 	days.f = days.g;
 	days.g = 0;
-	console.log("NEWDAY");
+	console.log('NEWDAY');
 }
 for (var e = 0; e < games.length; e++) {
 	totalkills += parseInt(games[e].kills);
@@ -64,51 +68,55 @@ for (var e = 0; e < games.length; e++) {
 	totalwins += parseInt(games[e].win);
 	totalbeds += parseInt(games[e].beds);
 }
-var winrate = totalwins / parseInt(localStorage.getItem("games"));
-winrate = Math.trunc(winrate * Math.pow(10, 2)) / Math.pow(10, 2);  //Truncates winrate
-//Update UI
-document.getElementById("totalkills").innerText = totalkills;
-document.getElementById("totalfinalkills").innerText = totalfinalkills;
-document.getElementById("totalwins").innerText = totalwins;
-document.getElementById("totalbeds").innerText = totalbeds;
-document.getElementById("winrate").innerText = winrate;
-document.getElementById("score").innerText = days.g;
-//Update chart
+var winrate = totalwins / parseInt(localStorage.getItem('games'));
+winrate = Math.trunc(winrate * Math.pow(10, 2)) / Math.pow(10, 2);  // Truncates winrate
+
+// Update UI
+document.getElementById('totalkills').innerText = totalkills;
+document.getElementById('totalfinalkills').innerText = totalfinalkills;
+document.getElementById('totalwins').innerText = totalwins;
+document.getElementById('totalbeds').innerText = totalbeds;
+document.getElementById('winrate').innerText = winrate;
+document.getElementById('score').innerText = days.g;
+
+// Update chart
 var bars = [];
 for (var i = 0; i < 7; i++) {
-	bars.push(document.getElementById("chart" + (i + 1)));
+	bars.push(document.getElementById('chart' + (i + 1)));
 }
-bars[0].style.height = ((days.a * 0.8) + 5) + "px";
-bars[1].style.height = ((days.b * 0.8) + 5) + "px";
-bars[2].style.height = ((days.c * 0.8) + 5) + "px";
-bars[3].style.height = ((days.d * 0.8) + 5) + "px";
-bars[4].style.height = ((days.e * 0.8) + 5) + "px";
-bars[5].style.height = ((days.f * 0.8) + 5) + "px";
-bars[6].style.height = ((days.g * 0.8) + 5) + "px";
-//Add a new game
-function addGame () {
+bars[0].style.height = ((days.a * 0.8) + 5) + 'px';
+bars[1].style.height = ((days.b * 0.8) + 5) + 'px';
+bars[2].style.height = ((days.c * 0.8) + 5) + 'px';
+bars[3].style.height = ((days.d * 0.8) + 5) + 'px';
+bars[4].style.height = ((days.e * 0.8) + 5) + 'px';
+bars[5].style.height = ((days.f * 0.8) + 5) + 'px';
+bars[6].style.height = ((days.g * 0.8) + 5) + 'px';
+
+// Add a new game
+function addGame() {
 	var game = {
-		kills: parseInt(document.getElementById("kills").value),
-		finalkills: parseInt(document.getElementById("finalkills").value),
-		win: parseInt(document.getElementById("won").value),
-		beds: parseInt(document.getElementById("beds").value)
+		kills: parseInt(document.getElementById('kills').value),
+		finalkills: parseInt(document.getElementById('finalkills').value),
+		win: parseInt(document.getElementById('won').value),
+		beds: parseInt(document.getElementById('beds').value)
 	};
-	localStorage.setItem(parseInt(localStorage.getItem("games")) + 1, JSON.stringify(game));
-	localStorage.setItem("games", parseInt(localStorage.getItem("games")) + 1);
-	//Update score
+	localStorage.setItem(parseInt(localStorage.getItem('games')) + 1, JSON.stringify(game));
+	localStorage.setItem('games', parseInt(localStorage.getItem('games')) + 1);
+	// Update score
 	days.g += (game.kills + (game.finalkills * 3) + (game.beds * 5) + (game.win * 7));
-	localStorage.setItem("days", JSON.stringify(days));
-	window.location = "";
+	localStorage.setItem('days', JSON.stringify(days));
+	window.location = '';
 }
-//Update date
-function saveDate () {
+
+// Update date
+function saveDate() {
 	var date = new Date();
 	var lastUse = {
 		d: date.getDate(),
 		m: date.getMonth(),
 		y: date.getYear()
 	};
-	localStorage.setItem("lastUse", JSON.stringify(lastUse));
-	window.setTimeout("saveDate()", 50);
+	localStorage.setItem('lastUse', JSON.stringify(lastUse));
+	setTimeout('saveDate()', 50);
 }
 saveDate();
